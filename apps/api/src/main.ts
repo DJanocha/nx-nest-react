b4e72c3e54e64f4af4aa39b56a8b825d6616bc8d
Daniel <daniel.jan.janocha@gmail.com>
2023-02-11 12:02:49 +0100

    api/added prisma

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,11 +5,14 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { PrismaService } from './prisma/prisma.service';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
