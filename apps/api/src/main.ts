3332f63389f3e7fc43cb607e90ad2c29ba1546bb
Daniel <daniel.jan.janocha@gmail.com>
2023-02-11 18:53:38 +0100

    api/set up swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { PrismaService } from './prisma/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -15,6 +16,17 @@ async function bootstrap() {
   await prismaService.enableShutdownHooks(app);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Katelier')
+    .setDescription('Your digital workshop')
+    .setVersion('0.1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, swaggerDocument);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
